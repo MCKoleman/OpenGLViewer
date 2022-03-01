1fb9f3565e8105b3c27a3b3968e7952386e53bba
MCKoleman <manu25025@gmail.com>
2022-03-01 17:30:42 -0500

    Finished project setup

--- src/OpenGLWarmup.h
+++ src/OpenGLWarmup.h
@@ -1,8 +1,2 @@
-﻿// OpenGLWarmup.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
+﻿#pragma once
 #include <iostream>
-
-// TODO: Reference additional headers your program requires here.
--- src/openGLInclude.h
+++ src/openGLInclude.h
@@ -1,7 +1,7 @@
+#pragma once
 #define GLEW_STATIC
 #include <GL/glew.h>
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/vec3.hpp>
-#pragma once
